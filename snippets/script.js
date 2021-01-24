692a06599154409448fbd57cd620879647c8a00e
Anwarul Islam <anwaarulislaam@gmail.com>
2021-01-25 04:06:27 +0600

    snippets

--- snippets/script.js
+++ snippets/script.js
@@ -1,16 +1,5 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        gistUrl: 'https://gist.github.com/anwarulislam/247e19ea282d1bb82cbad45489635fdb'
-    },
-    render: function () {
-        var el = document.getElementById('scr')
 
-        console.log(this.gistUrl)
 
-        let recaptchaScript = document.createElement('script')
-        recaptchaScript.setAttribute('src', this.gistUrl)
-        el.appendChild(recaptchaScript)
-
-    }
-})
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get('id');
+postscribe('#scr', `<script src="https://gist.github.com/anwarulislam/${id}.js"></script>`);
